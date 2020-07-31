4bb51552b00eacf324a54913aff26a8c523eb47c
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-31 12:04:32 -0400

    updated

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -12,8 +12,8 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                    <a className="nav-link" href="/">Overview</a>
-                </li>
+                    <a className="nav-link" href="/">Login</a>
+                </li> 
                 <li className="nav-item">
                     <a className="nav-link" href="/">My Account</a>
                 </li>
@@ -23,7 +23,7 @@ function Navbar() {
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+                    <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Login</button>
                 </form>
             </div>
         </nav>
