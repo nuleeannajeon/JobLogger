ab4154f5ec59a1004448c4fbe4111fcc42c734d7
JamesLee <48775473+titanian229@users.noreply.github.com>
2020-08-04 16:39:42 -0400

    Fixed navbar not toggling, added icon image to registration and login pages

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,15 +6,6 @@ import './styles.css';
 const LoginButton = () => <Link to="/login" className="btn btn-outline-dark my-2 my-sm-0" type="submit">Login</Link>
 const LogoffButton = () => <Link to="/logout" className="btn btn-outline-dark my-2 my-sm-0" type="submit">Logout</Link>
 
-// function Navbar() {
-
-//     return (
-//         <nav className="navbar navbar-expand-lg">
-//             <Link className="navbar-brand" to="/">JobLogger</Link>
-//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-
-
 function Navbar() {
     const [globalStore] = useGlobalStore();
 
@@ -27,7 +18,7 @@ function Navbar() {
                 className="navbar-toggler"
                 type="button"
                 data-toggle="collapse"
-                data-target="#navbarSupportedContent"
+                data-target="#navbar"
                 aria-controls="navbarSupportedContent"
                 aria-expanded="false"
                 aria-label="Toggle navigation"
@@ -37,7 +28,7 @@ function Navbar() {
                 </span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse" id="navbar">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         <Link className="nav-link" to="/overview">
