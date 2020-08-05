d6410b589b36da2dca4e7056f6b65051c892a195
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-08-05 13:25:41 -0400

    added search files

---
+++ client/src/pages/Search.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar/index';
+import SearchForm from '../components/SearchForm/index';
+import API from '../utils/searchAPI';
+import Container from '../components/Container/index';
+
+
+class Search extends Component {
+    state = {
+        results: [],
+        search: ""
+    };
+
+    componentDidMount() {
+        this.searchJobs();
+    };
+
+    searchJobs = query => {
+        API.searchJob(query)
+            .then(res => this.setState({ results: res.data}))
+            .catch(err => console.log(err));
+    }
+
+    handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.searchJobs(this.state.search);
+    };
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <SearchForm 
+                    value={this.state.search}
+                    handleInputChange={this.handleInputChange}
+                    handleFormSubmit={this.handleFormSubmit}
+                />
+                <Container 
+                    results={this.state.results}
+                />
+            </div>
+        );
+    }
+};
+
+export default Search;
