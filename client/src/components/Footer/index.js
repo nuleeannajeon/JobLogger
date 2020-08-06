adb95df6caea31420b03f5a4fa4fcf8c631f0555
JamesLee <48775473+titanian229@users.noreply.github.com>
2020-08-06 12:00:09 -0400

    Added footer to bottom of page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     footer: {
         // position
-    }
-}))
-
+        padding: '5px 15vw',
+        backgroundColor: theme.palette.primary.main,
+        color: 'white',
+        // marginTop: '2em',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+}));
 
 const Footer = () => {
-    const classes = useStyles()
-    return (
-        <div className={classes.footer}>
-            	&copy; 2020 League of Coders
-        </div>
-    )
-}
+    const classes = useStyles();
+    return <div className={classes.footer}>&copy; 2020 League of Coders</div>;
+};
 
 export default Footer;
